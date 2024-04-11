05963e324094f9971e7f788afe0924df3b3fbd29
Gabriel Rockson <gabrielrockson99@gmail.com>
2024-04-11 22:07:50 +0000

    chore: add webstorm IDE config

---
+++ app/components/PrioritizedList.tsx
@@ -0,0 +1,11 @@
+const PrioritizedList = () => {
+  return (
+    <div className={""}>
+      <h3 className={"text-gray-800 font-semibold text-center"}>
+        Your Tasks Prioritized
+      </h3>
+    </div>
+  );
+};
+
+export default PrioritizedList;
